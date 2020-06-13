22b9cfad0f2c1c8db2b2cc335bf5db13982a77e9
Gustavo Castanho <duartecgustavo@outlook.com>
2020-06-13 19:49:43 -0300

    Desafios Python 61 - 70
    Upload dos dois que restavam.
    Desafio 69 e desafio 70.

---
+++ Desafios/Ex069CADASTROwhile.py
@@ -0,0 +1,35 @@
+# Desafio 69 - Aula 15 : Programa que leia a IDADE, SEXO de varias pessoas e me pergunte se o usuario deseja continuar.
+# Ao fim mostre quantas pessoas tem  mais de 18 anos / Quantos homens foram cadastrados / Qauntas mulheres  tem menos de 20 anos;
+age = countage = countman = countwoman = 0
+sex = choice = ' '
+
+while True:
+
+    print('=-' * 25)
+    print(f'{"CADASTRO DE PESSOAS!":^50}')
+    print('-=' * 25)
+
+    age = int(input('Sua idade: '))
+    sex = str(input('Seu sexo[H/M]: ')).upper()
+    while sex not in 'HM':
+        sex = str(input('Opção invalida! Seu sexo[H/M]: ')).upper()
+
+    if age > 18:
+        countage += 1
+    if sex in 'H':
+        countman+=1
+    if age < 20 and sex in 'M':
+        countwoman+=1
+
+    choice = str(input('Quer continuar?[S/N] ')).upper()
+    while choice not in 'SN':
+        choice = str(input('Opção invalida! Quer continuar?[S/N] ')).upper()
+    if choice in 'N':
+        break
+
+print('-='*25)
+print(f'{"DADOS!":^50}')
+
+print(f'Pessoas maiores de idade: {countage}')
+print(f'Homens: {countman}')
+print(f'Mulheres menores que 20 anos: {countwoman}')
---
+++ Desafios/Ex070PRODUTOS.py
@@ -0,0 +1,36 @@
+# Desafio 70 - Aula 15 : Programa que leia o NOME e o PREÇO de varios produtos, o programa deve perguntar se quer continuar.
+# Instruções: A/ Qual o total gasto na compra | B/ Quantos produtos custam mais de R$1000 | C/ Qual o nome do produto mais barato.
+
+print('-='*25)
+print(f'\033[32m{"LEITOR DE PRODUTOS":^50}\033[m')
+print('=-'*25)
+
+tot = tot1000 = menor = count = 0
+namemenor = ' '
+while True:
+    print('\033[31m-=\033[m' * 25)
+    print(f'{"PRODUTO":^50}')
+    count +=1
+
+    name = str(input('Nome do produto: ')).lower()
+    price = float(input('Preço do produto: R$'))
+
+    tot +=price
+    if price > 1000:
+        tot1000 +=1
+    if count == 1 or price < menor:
+        menor = price
+        namemenor = name
+
+    choice = str(input('Quer continuar?[S/N] ')).upper()[0]
+    while choice not in 'SN':
+        choice = str(input('\033[31mOpção invalida!\033[m Quer continuar?[S/N] ')).upper()[0]
+
+    if choice in 'N':
+        print('Finalizando programa!.. ')
+        break
+
+print('-='*25)
+print(f'Total gasto: \033[34mR${tot:.2f}\033[m.')
+print(f'No total {tot1000} produtos custaram mais de \033[31mR$1000.00\033[m.')
+print(f'O produto mais barato foi o \033[34m{namemenor}\033[m que custou apenas \033[32mR${menor:.2f}\033[m.')
