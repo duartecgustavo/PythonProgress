e8dacd0c2c1ac37ed68f2c593e8b29a8a308751d
Gustavo Castanho <65131471+duartecgustavo@users.noreply.github.com>
2020-07-02 16:37:13 -0300

    att desafio

---
+++ desafios/Mundo 3/Ex074.py
@@ -0,0 +1,10 @@
+# Desafio 74 - Aula 16 : Programa que deve gerar 5 numeros aleatórios e adicionalos a uma TUPLA.
+# Depois disso  mostre a listagem dos numeros gerados e também indique qual é o MAIOR e qual é o MENOR.
+
+from random import randint
+
+tupla = ((randint(1,10)),(randint(1,10)),(randint(1,10)),(randint(1,10)),(randint(1,10)))
+
+for n in tupla:
+    print(n, end=' ')
+print(f'\nO maior valor da tupla é {max(tupla)} e o menor é {min(tupla)}.')
