81b648cbf3374db978cefa9fda8544d4bc470fdf
Gustavo Castanho <65131471+duartecgustavo@users.noreply.github.com>
2020-08-15 18:17:45 -0300

    Update Ex090DICIONARIOS.py

--- desafios/Mundo 3/Ex090DICIONARIOS.py
+++ desafios/Mundo 3/Ex090DICIONARIOS.py
@@ -1,4 +1,4 @@
-# Desafio 90 - Aula 19 :  Programa que leia o NOME e a MÉDIA de um aluno apenas. Guarde também a SITUAÇÃO em um DICIONARIO.
+# Desafio 90 - Aula 19 :  Programa que leia o nome e a média, analize sua situação (APROVADO/REPROVADO) e armazene tudo em um dicionario.
 # Depois mostre o conteúdo da estrutura na tela.
 
 aluno = {}
@@ -7,7 +7,7 @@ aluno['Nome'] = str(input('Seu nome: '))
 
 aluno['Média'] = float(input('Sua média: '))
 while aluno['Média'] < 0 or aluno['Média'] > 10:
-    aluno['Média'] = float(input('Impossivel!\nSua média: '))
+    aluno['Média'] = float(input('Impossivel!\nSua média correta: '))
 
 if aluno['Média'] <= 5:
     aluno['Situação'] = ("REPROVADO!")
