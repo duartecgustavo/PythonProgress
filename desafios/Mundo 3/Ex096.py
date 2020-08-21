b0ac109ffb7ba6d339e37235f733780566a0789d
duartecgustavo@outlook.com <duartecgustavo@outlook.com>
2020-08-21 15:48:43 -0300

    Update Ex096.py

--- desafios/Mundo 3/Ex096.py
+++ desafios/Mundo 3/Ex096.py
@@ -1,9 +1,10 @@
 # Desafio 97 - Aula 20: Receba um texto qualquer como parametrô e mostre a mensagem de forma adaptavel:
 
 def escreva(msg):
-    print('-'*30)
-    print(f'{msg:^30}')
-    print('-'*30)
+    tam = len(msg) + 4
+    print('-'*tam)
+    print(f'  {msg}')
+    print('-'*tam)
 
 for count in range(0,3):
     escreva(msg = str(input('Escreva uma mensagem: ')))
