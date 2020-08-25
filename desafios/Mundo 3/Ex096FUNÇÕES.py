9e9804fba44ec06c07604a468f3ed61546a8f87a
duartecgustavo@outlook.com <duartecgustavo@outlook.com>
2020-08-25 16:52:17 -0300

    att

--- desafios/Mundo 3/Ex096FUNÇÕES.py
+++ desafios/Mundo 3/Ex096FUNÇÕES.py
@@ -1,9 +1,9 @@
-# Desafio 96 - Aula 20: Programa que leeia a LARGURA e ALTURA de um terreno e armazene em uma FUNÇÃO chmada area();
+# Desafio 96 - Aula 20: Programa que leia a LARGURA e ALTURA de um terreno e armazene em uma FUNÇÃO chamada área();
 # Depois apresente a área do terreno.
 
 def area(a , l):
-    soma= a + l
-    print(f'A área do terreno, seguindo a Largura de {l} e a Altura de {a} é iagual à {soma}:')
+    ar = a * l
+    print(f'A área do terreno, seguindo a Largura de {l} e a Altura de {a} é igual à {ar}m²:')
 
 
 a=(float(input('Altura: ')))
