5d82aa760874a54bdf8a32b68c8dbfbcad6b18f9
duartecgustavo@outlook.com <duartecgustavo@outlook.com>
2020-08-26 10:34:02 -0300

    Desafio 100
    chegueeei

---
+++ desafios/Mundo 3/Ex0100.py
@@ -0,0 +1,26 @@
+# Desafio 100 - Aula 20: Programa que tenha uma lista chamada 'numeros' e duas funções, chamadas sorteio() e somaPar().
+# A função sorteio irá sortear 5 numeros e adicionalos dentro da lista.
+# E a função somaPar() irá somar os valores pares dentro desta lista.
+
+from random import randint
+
+lista = [0, 0, 0, 0, 0]
+
+
+def sorteio(lst):
+    print('Sorteando os 5 valores da lista:', end=' ')
+    for count in range(0,5):
+        lst[count] = randint(0,10)
+    print(lst, 'PRONTO!')
+
+def somaPar(lst):
+    soma=0
+    print(f'Somando os valores pares de {lista}, temos',end=' ')
+    for valor in lst:
+        if valor % 2 ==0:
+            soma += valor
+    print(soma)
+
+
+sorteio(lista)
+somaPar(lista)
