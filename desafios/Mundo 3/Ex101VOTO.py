d296e4c654e69c932f85ac029fa84a4c4392abbf
duartecgustavo@outlook.com <duartecgustavo@outlook.com>
2020-09-06 13:32:40 -0300

    Update Ex101VOTO.py

--- desafios/Mundo 3/Ex101VOTO.py
+++ desafios/Mundo 3/Ex101VOTO.py
@@ -1,29 +1,31 @@
 # Desafio 101 - Aula 21: Programa que valide o voto pela idade.
 # Deve possuir uma FUNÇÃO() chamada voto() que vai receber  o parametro Ano de Nascimento
 # Depois retornara um valor literal que pode ser NEGADO, OPCIONAL, OBRIGATÓRIO nas eleições
-
-from datetime import date
-
 def voto(ano = 0):
-    if idade < 18:
-        print('VOTO NEGADO!')
+    from datetime import date
+    idade = date.today().year - ano
+    print(f'Com {idade} anos: ',end=' ')
+    ''' def voto() quando chamada retornará a situação do indivíduo em reçalção ao voto.'''
+    if ano <= 0 or ano > date.today().year:
+        condicao = 'ANO INVÁLIDO'
+    elif idade < 18:
+        condicao = 'VOTO NEGADO!'
     elif idade < 65:
-        print('VOTO OBRIGATÓRIO!')
+        condicao = 'VOTO OBRIGATÓRIO!'
     else:
-        print('VOTO OPCIONAL!')
+        condicao = 'VOTO OPCIONAL!'
+    return condicao
 
 while True:
 
     print('-'*50)
-
     ano = int(input('Ano de Nascimento: '))
-
-    idade = date.today().year - ano
-    print(f'Com {idade} anos: ',end=' ')
-    voto(ano)
+    print(voto(ano))
 
     choice = str(input('Quer continuar? [S/N]')).upper()[0]
     while choice not in 'SN':
         choice=str(input('Opção invalida!\nQuer continuar? [S/N]')).upper()[0]
     if choice in 'N':
         break
+
+print('Programa Finalizado!')
