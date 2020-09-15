6d0a549e44d5ac7637ed017ebc9141670725d24f
duartecgustavo@outlook.com <duartecgustavo@outlook.com>
2020-09-15 14:10:12 -0300

    add

--- conteudo/complementares/praticando.py
+++ conteudo/complementares/praticando.py
@@ -1,16 +1,3 @@
-def leiaInt(msg):
-    ok = False
-    valor = 0
-    while True:
-        numero = str(input(msg))
-        if numero.isnumeric():
-            ok = True
-            valor = int(numero)
-        else:
-            print('ERROO! Diga um numero inteiro valido!')
-        if ok == True:
-            break
-    return valor 
-    
-numero = leiaInt('Diga um numero: ')
-print(f'O numero digitado foi {numero}.')
+choice = str(input('Quer continuar? [S/N]')).upper()[0]
+while choice not in 'SN' or choice.isalpha() == False:
+    choice = str(input('Opção invalida!\nQuer continuar? [S/N]')).upper()[0]
